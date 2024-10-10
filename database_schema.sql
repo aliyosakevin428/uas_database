766e04afc097fc9175ac18d6171c0c34d15c2302
Kevin420 <155149448+aliyosakevin428@users.noreply.github.com>
2024-10-11 01:43:04 +0800

    ...

--- database_schema.sql
+++ database_schema.sql
@@ -18,4 +18,3 @@ insert into user (nama, telepon) values ("kevin", "3232323232");
 
 insert into post (judul, tulisan, user_id) values ("King Garet Purple", "So Freeze", 1);
 insert into post (judul, tulisan, user_id) values ("Dunhill Black", "Feel Bold", 2);
-
